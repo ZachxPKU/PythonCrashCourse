9d5e9ccfb374ea9eada0a32ad4cc8f988fb24d96
Zach <18819008321@qq.com>
2022-09-18 11:35:11 +0800

    Chapter04 4.5 4.6 4.7

---
+++ Chapter04/buffet.py
@@ -0,0 +1,9 @@
+foods = ('salmon', 'abalone', 'beef', 'chicken', 'goose liver')
+for food in foods:
+    print(food)
+
+# food[0] = 'fish'
+
+foods = ('tuna', 'abalone', 'beef', 'su shi', 'goose liver')
+for food in foods:
+    print(food)
